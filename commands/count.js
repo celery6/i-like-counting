a7529b8ca396d42bfbc2fcc77bccbc8f077fa381
Celery <the.real.paintblob@gmail.com>
2021-04-08 21:24:40 -0400

    mysQL

--- commands/count.js
+++ commands/count.js
@@ -9,7 +9,6 @@ module.exports = {
 		let count1 = {};
 		let count2 = {};
 
-		//DATABASE!!
 
 		client.on('message', message => {
 			fs.readFile('./theCount.json', 'utf8', (err, jsonString) => {   //check if stopped ?
