4a9d9e24273a8fd8e41b40436ff6c8d4472be89a
Celery <the.real.paintblob@gmail.com>
2021-04-12 00:30:35 -0400

    pretty

--- bot.js
+++ bot.js
@@ -1,102 +1,104 @@
-const fs = require("fs");
-const Discord = require("discord.js");
-const { prefix, token, password } = require("./config.json");
-const client = new Discord.Client();
-client.commands = new Discord.Collection();
-const whitelist = ["306529453826113539", "682042083024044161"]; //whitelist
-const mysql = require("mysql");
+const fs = require('fs')
+const Discord = require('discord.js')
+const { prefix, token, password } = require('./config.json')
+const client = new Discord.Client()
+client.commands = new Discord.Collection()
+const whitelist = ['306529453826113539', '682042083024044161'] //whitelist
+const mysql = require('mysql')
 const commandFiles = fs
-  .readdirSync("./commands")
-  .filter((file) => file.endsWith(".js"));
-const cooldowns = new Discord.Collection();
+    .readdirSync('./commands')
+    .filter((file) => file.endsWith('.js'))
+const cooldowns = new Discord.Collection()
 
 for (const file of commandFiles) {
-  const command = require(`./commands/${file}`);
-  client.commands.set(command.name, command);
+    const command = require(`./commands/${file}`)
+    client.commands.set(command.name, command)
 }
 
 const con = mysql.createConnection({
-  //mysql connect
-  host: "localhost",
-  user: "root",
-  password: password,
-  database: "uwu",
-});
+    //mysql connect
+    host: 'localhost',
+    user: 'root',
+    password: password,
+    database: 'uwu',
+})
 
 con.connect(function (err) {
-  if (err) throw err;
-  console.log("Connected!");
-});
+    if (err) throw err
+    console.log('Connected!')
+})
 
-client.on("message", (message) => {
-  const security = message.content.toLowerCase().trim().split(" "); //security re sponse
-  if (security.includes("name") && security.includes("school")) {
-    return message.reply(
-      'MY NAME IS "BOTBOT", I ATTEND OTHER SECONDARY SCHOOL'
-    );
-  }
-
-  if (message.author.bot) return; //exit if bot msg
-  if (!message.content.startsWith(prefix)) return;
-
-  const args = message.content.slice(prefix.length).trim().split(/ +/);
-  const commandName = args.shift().toLowerCase();
-  const command =
-    client.commands.get(commandName) ||
-    client.commands.find(
-      (cmd) => cmd.aliases && cmd.aliases.includes(commandName)
-    );
-
-  if (!command) return;
-
-  if (command.noAdmin === false) {
-    //check whitelist
-    if (!whitelist.includes(message.author.id)) {
-      //If person is not authorized, send msg
-      const emoji = client.emojis.cache.get("801510839386374214");
-      message.reply(
-        `HA ur not permitted to use this lOLLOLOLLOLO FAILURE ${emoji}`
-      );
-      return;
+client.on('message', (message) => {
+    const security = message.content.toLowerCase().trim().split(' ') //security re sponse
+    if (security.includes('name') && security.includes('school')) {
+        return message.reply(
+            'MY NAME IS "BOTBOT", I ATTEND OTHER SECONDARY SCHOOL'
+        )
     }
-  }
 
-  if (!cooldowns.has(command.name)) {
-    cooldowns.set(command.name, new Discord.Collection());
-  }
+    if (message.author.bot) return //exit if bot msg
+    if (!message.content.startsWith(prefix)) return
 
-  const now = Date.now();
-  const timestamps = cooldowns.get(command.name);
-  const cooldownAmount = command.cooldown * 1000;
+    const args = message.content.slice(prefix.length).trim().split(/ +/)
+    const commandName = args.shift().toLowerCase()
+    const command =
+        client.commands.get(commandName) ||
+        client.commands.find(
+            (cmd) => cmd.aliases && cmd.aliases.includes(commandName)
+        )
 
-  if (timestamps.has(message.guild.id)) {
-    const expirationTime = timestamps.get(message.guild.id) + cooldownAmount;
+    if (!command) return
 
-    if (now < expirationTime) {
-      if (command.name === "count")
-        return message.reply("ALREADY COUNTING IDOT");
-
-      const timeLeft = (expirationTime - now) / 1000;
-      return message.reply(
-        `please wait ${timeLeft.toFixed(
-          1
-        )} more second(s) before reusing the \`${command.name}\` command.`
-      );
+    if (command.noAdmin === false) {
+        //check whitelist
+        if (!whitelist.includes(message.author.id)) {
+            //If person is not authorized, send msg
+            const emoji = client.emojis.cache.get('801510839386374214')
+            message.reply(
+                `HA ur not permitted to use this lOLLOLOLLOLO FAILURE ${emoji}`
+            )
+            return
+        }
     }
-  }
 
-  timestamps.set(message.guild.id, now);
-  setTimeout(() => timestamps.delete(message.guild.id), cooldownAmount);
+    if (!cooldowns.has(command.name)) {
+        cooldowns.set(command.name, new Discord.Collection())
+    }
 
-  try {
-    command.execute(message, args, client, con);
-  } catch (error) {
-    console.error(error);
-    message.reply("ERROR HAPPENED IDOT!");
-  }
-});
+    const now = Date.now()
+    const timestamps = cooldowns.get(command.name)
+    const cooldownAmount = command.cooldown * 1000
 
-client.login(token);
-client.once("ready", () => {
-  console.log("uwu im here");
-});
+    if (timestamps.has(message.guild.id)) {
+        const expirationTime = timestamps.get(message.guild.id) + cooldownAmount
+
+        if (now < expirationTime) {
+            if (command.name === 'count')
+                return message.reply('ALREADY COUNTING IDOT')
+
+            const timeLeft = (expirationTime - now) / 1000
+            return message.reply(
+                `please wait ${timeLeft.toFixed(
+                    1
+                )} more second(s) before reusing the \`${
+                    command.name
+                }\` command.`
+            )
+        }
+    }
+
+    timestamps.set(message.guild.id, now)
+    setTimeout(() => timestamps.delete(message.guild.id), cooldownAmount)
+
+    try {
+        command.execute(message, args, client, con)
+    } catch (error) {
+        console.error(error)
+        message.reply('ERROR HAPPENED IDOT!')
+    }
+})
+
+client.login(token)
+client.once('ready', () => {
+    console.log('uwu im here')
+})
